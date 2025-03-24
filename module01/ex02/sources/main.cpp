7da08e857dc31b78a22465555db766c69cbe4cb8
mendes-jv <mendes.silva.jv@gmail.com>
2025-03-24 15:55:53 -0300

    feat(cpp01-ex02): Implements the files and exercise

---
+++ module01/ex02/sources/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    std::string brain = "HI THIS IS BRAIN";
+
+    const std::string *stringPTR = &brain;
+    const std::string &stringREF = brain;
+
+    std::cout << "Addresses:" << std::endl;
+    std::cout << &brain << std::endl;
+    std::cout << stringPTR << std::endl;
+    std::cout << &stringREF << std::endl;
+
+    std::cout << "\nValues:" << std::endl;
+    std::cout << brain << std::endl;
+    std::cout << *stringPTR << std::endl;
+    std::cout << stringREF << std::endl;
+}
