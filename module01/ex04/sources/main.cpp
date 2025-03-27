6bb3c1cdda0a40efe510796ccf9aa4acfe502b82
mendes-jv <mendes.silva.jv@gmail.com>
2025-03-27 17:06:56 -0300

    feat(cpp01-ex04): Implements exercise

--- module01/ex04/sources/main.cpp
+++ module01/ex04/sources/main.cpp
@@ -4,68 +4,45 @@
 #include <cstdio>
 #include <cstdlib>
 
-std::string my_replace(std::string buffer, const std::string search, const std::string replace)
-{
-	size_t	erase_length = search.length();
-	size_t	replace_length = replace.length();
-	size_t	pos_search = 0;
-
-	pos_search = buffer.find(search);
-	while (pos_search != std::string::npos)
-	{
-		buffer.erase(pos_search, erase_length);
-		buffer.insert(pos_search, replace);
-		pos_search = buffer.find(search, pos_search + replace_length);
-	}
-	return (buffer);
-}
-
-int main(int argc, char **argv)
-{
-	if (argc != 4)
-	{
-		std::cout << "wrong number of arguments" << std::endl <<
-		"Usage:\t./replace <file_name> <search> <replace>" << std::endl;
-	}
-	else
-	{
-		const std::string filename = argv[1];
-		const std::string	search = argv[2];
-		const std::string	replace = argv[3];
-		std::string			buffer;
-		std::ifstream		infile;
-		std::ofstream		outfile;
-
-		infile.open(argv[1]);
-		if (infile.is_open() == true)
-		{
-			outfile.open(filename + ".replace", std::ios::out | std::ios::trunc ); //only works on macOS with the +, to make it work on Linux, comment out "filename +"
-			if (outfile.is_open() == true)
-			{
-				while (std::getline(infile, buffer))
-				{
-					buffer = my_replace(buffer, search, replace);
-					outfile << buffer;
-					if (infile.peek() != EOF)
-						outfile << std::endl;
-				}
-				outfile.close();
-			}
-			else
-			{
-				std::perror("Outfile Error");
-				infile.close();
-				return (EXIT_FAILURE);
-			}
-			infile.close();
-		}
-		else
-		{
-			std::perror("Infile Error");
-			std::cout << "Outfile not created or truncated." << std::endl;
-			return (EXIT_FAILURE);
-		}
-		return (EXIT_SUCCESS);
-	}
-	return (EXIT_FAILURE);
+int main(int argc, char **argv) {
+    if (!argv || argc != 4 || !argv[0] || !argv[1] || !argv[2] || !argv[3]) {
+        std::cerr << "Error: wrong number of arguments\nUsage: "
+                "./replace <file_name> <string_to_search> <string_to_replace>"
+                << std::endl;
+        return (EXIT_FAILURE);
+    }
+    const std::string inputFilename = argv[1];
+    std::ifstream inputFile(inputFilename.c_str(), std::ios::in);
+    if (!inputFile) {
+        std::cerr << "Error: could not open file \'" << inputFilename << "\'" << std::endl;
+        return (EXIT_FAILURE);
+    }
+    const std::string outputFilename = inputFilename + ".replace";
+    std::ofstream outputFile(outputFilename.c_str(), std::ios::out | std::ios::trunc);
+    if (!outputFile) {
+        std::cerr << "Error: \'" << outputFilename << "\' was not created or truncated" << std::endl;
+        inputFile.close();
+        return (EXIT_FAILURE);
+    }
+    std::string content;
+    const std::string stringToSearch = argv[2];
+    const std::string stringToReplace = argv[3];
+    if (stringToSearch.empty() || stringToReplace.empty()) {
+        std::cerr << "Error: string to search and/or string to replace is empty" << std::endl;
+        inputFile.close();
+        outputFile.close();
+        return (EXIT_FAILURE);
+    }
+    while (std::getline(inputFile, content)) {
+        size_t linePosition = 0;
+        while ((linePosition = content.find(stringToSearch, linePosition)) != std::string::npos) {
+            content.erase(linePosition, stringToSearch.length());
+            content.insert(linePosition, stringToReplace);
+            linePosition += stringToReplace.length();
+        }
+        outputFile << content << std::endl;
+    }
+    inputFile.close();
+    outputFile.close();
+    return (EXIT_SUCCESS);
 }
